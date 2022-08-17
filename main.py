bcaecc6467d23319174c071fe27782336e11c0fe
cryptomafiaPB <71580282+cryptomafiaPB@users.noreply.github.com>
2022-08-17 22:01:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+def stored():
+    use = input("Enter a username: ")
+    pas = input("Enter a pasword: ")
+
+    with open('pas.txt', 'a') as f:
+        print("\n" + f.write(str(use) + ' | ' + str(pas)))
+    
+def view():
+    with open('pas.txt', 'r') as f:
+        for i in f.readlines():
+            u = i.split('|')
+            print('\nUSERNAME::' + str(u[0]) + '  PASSWORD::' + str(u[1]))
+
+
+sp = 'RAM RAM'
+print(sp)
+op = input("Choose only one option [add/view]: ")
+if op=="add":
+    stored()
+    input()
+else:
+    view()
+    input()
+
